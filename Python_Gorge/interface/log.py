01e5d5409fb9d7d8793226d9e5e8208c6eead29c
user-chuanrong123 <1021719031@qq.com>
2021-10-09 22:59:56 +0800

    更新串口界面1009

--- Python_Gorge/interface/log.py
+++ Python_Gorge/interface/log.py
@@ -1,53 +1,59 @@
 """
-日志文件--GUI界面
+日志
+日志的级别排序: CRITICAL > ERROR > WARNING > INFO > DEBUG
 """
 
 import logging
-
-class MyLog:
-    #执行日志函数
-    def do_log(self, msg, level):
-        #创建一个容器，存放日志
-        my_log = logging.getLogger('GUI调试')
-        #设定等级
-        my_log.setLevel('REEOR')
-        #定义输出格式
-        myFormatter = logging.Formatter('%(asctime)s %(filename)s [line:%(lineno)d] %(name)s: %(message)s')
-        #创建输出渠道
-        ch = logging.StreamHandler() #输出到控制台
-        ch.setLevel('DEBUG')
-        ch.setFormatter(myFormatter) #设置输出格式
-        fh = logging.FileHandler(r'E:\SOFT_GX96\text_log.txt', encoding='utf-8')
-        fh.setLevel('DEBUG')
-        fh.setFormatter(myFormatter)  # 设置输出格式
-        #指定我们容器中日志的输出渠道
-        my_log.addHandler(ch)
-        my_log.addHandler(fh)
+import sys
+from settings import LOG_LEVEL,LOG_FMT,LOG_DATEFMT,LOG_FILENAME
 
 
-        if level == 'DEBUG':
-            my_log.debug(msg)
-        if level == 'INFO':
-            my_log.info(msg)
-        if level == 'ERROR':
-            my_log.error(msg)
-        if level == 'WARNING':
-            my_log.warning(msg)
-        if level == 'CRITICAL':
-            my_log.critical(msg)
+class Logger(object):
 
-        #关闭渠道
-        my_log.removeHandler(ch)
-        my_log.removeHandler(fh)
+    def __init__(self):
+        #1:获取一个logger对象
+        self._logger = logging.getLogger()
+        #定义日志输出格式
+        self.formatter = logging.Formatter(fmt=LOG_FMT, datefmt=LOG_DATEFMT)
+        #指定日志输出渠道
+        self._logger.addHandler(self._get_file_handler(LOG_FILENAME))
+        self._logger.addHandler(self._get_console_handler())
+        #设置日志文件的等级
+        self._logger.setLevel(LOG_LEVEL)
 
+
+    def _get_file_handler(self, filename):
+        """返回一个文件日志handler"""
+        #1:获取一个文件日志handler
+        filehandler = logging.FileHandler(filename=filename, encoding='utf-8')
+        #2:设置日志格式
+        filehandler.setFormatter(self.formatter)
+        #3:返回
+        return filehandler
+
+
+    def _get_console_handler(self):
+        """返回一个输出到终端日志handler"""
+        #1:获取一个输出到终端的handler
+        console_handler = logging.StreamHandler(sys.stdout)
+        #2:设置日志格式
+        console_handler.setFormatter(self.formatter)
+        #3:返回handler
+        return console_handler
+
+
+    @property # https://zhuanlan.zhihu.com/p/64487092
+    def logger(self):
+        return self._logger
+
+
+#初始化并配一个logger对象
+#使用时，直接导入logger就可用使用
+logger = Logger().logger
 
 if __name__ == '__main__':
-    MyLog().do_log('加油', 'ERROR')
-
-
-
-
-
-
-
-
+    logger.debug('调试信息')
+    logger.info('状态信息')
+    logger.warning('警告信息')
+    logger.error('错误信息')
+    logger.critical('严重错误信息')
--- Python_Gorge/settings.py
+++ Python_Gorge/settings.py
@@ -1,31 +1,45 @@
-
-#字体及大小
+##############串口界面设置信息 ################
+#设置界面字体
 FONT_AND_SIZE=('宋体', 16)
 FONT_BUT_SIZE=('宋体', 14)
 
-#串口号数组
+#设置串口号数组
 GORGE_ARRAY = ['COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7']
-#默认串口
+#设置默认串口
 DEFAULT_GORGE = 'COM7'
 
-#选择窗口大小
+#设置选择窗口大小
 SELSIZE = (15, 9)
 
 
-#波特率数组
+#设置波特率数组
 BAUD_ARRAY = ['1200', '2400', '9600', '19200', '38400', '115200']
-#默认大小
+#设置波特率默认大小
 DEFAULT_SIZE = '9600'
 
-#数据位列表
+#设置数据位列表
 DATA_BIT = ['5', '6', '7', '8']
+#设置数据位默认大小
 DEFAULT_DATA_BIT = '8'
-#停止位列表
+
+#设置停止位列表
 STOP_BIT = ['1', '1.5', '2']
+#设置停止位默认大小
 DEFAULT_STOP_BIT = '1'
 
-#发送数据窗口大小
+#设置发送数据的写入窗口大小
 SEND_DATA_SIZE=(16, 8)
 
-#右侧Frame框架大小
+#设置右侧Frame框架大小
 FRAME_SIZE=(50, 6)
+
+
+##############日志信息################
+
+import logging
+#默认的配置
+LOG_LEVEL = logging.ERROR  #设置默认等级
+#filename:指文件名称：这个日志是哪个文件输出的
+LOG_FMT = '%(asctime)s %(filename)s [line:%(lineno)d] %(name)s: %(message)s'
+LOG_DATEFMT = '%Y-%m-%d %H:%M:%S' #默认时间格式
+LOG_FILENAME = 'gorge.log' #日志文件名称
