1234195d37813959287d7950cadc434092c3766c
user-chuanrong123 <1021719031@qq.com>
2021-10-13 23:30:02 +0800

    1013

--- Python_Gorge/interface/inter_face.py
+++ Python_Gorge/interface/inter_face.py
@@ -18,6 +18,7 @@ from settings import DEFAULT_STOP_BIT
 from settings import SEND_DATA_SIZE
 from settings import FRAME_SIZE
 from settings import CHECKOUT
+from port_package import Communication
 
 #左侧框架设计
 frame_layout1 = [
@@ -28,30 +29,57 @@ frame_layout1 = [
 	    [sg.Text('停止位:', font=FONT_AND_SIZE), sg.Combo(STOP_BIT, font=FONT_BUT_SIZE, key='-STOP_BIT-', default_value=DEFAULT_STOP_BIT, size=SELSIZE)],
 	    [sg.Button('打开串口', font=FONT_BUT_SIZE, key='-OPEN_BUTTEN-'), sg.Button('关闭串口', font=FONT_BUT_SIZE, key='-CLOSE_BUTTEN-')],
 	    [sg.Text('')],
-	    [sg.Button('发送', font=FONT_BUT_SIZE), sg.Input(size=SEND_DATA_SIZE, font=FONT_BUT_SIZE, key='-INPUT_TEXT-')],
-	    [sg.Button('接收', font=FONT_BUT_SIZE)],
+	    [sg.Button('发送', font=FONT_BUT_SIZE, key='-SEND_DATA-'), sg.Input(size=SEND_DATA_SIZE, font=FONT_BUT_SIZE, key='-INPUT_TEXT-')],
+	    [sg.Button('接收', font=FONT_BUT_SIZE, key='-RECIPT_DATA-')]
 ]
 
 #右侧框架设计
 frame_layout2 = [
-	[sg.Text('发送数据:', font=FONT_BUT_SIZE), sg.Button('清除发送数据', font=FONT_BUT_SIZE, key='-CLEARBUTTER-')],
+	[sg.Text('发送数据:', font=FONT_BUT_SIZE), sg.Button('清除发送数据', font=FONT_BUT_SIZE, key='-CLEAR-SENDDATA')],
 	[sg.Listbox('', size=FRAME_SIZE, key='-PB2-')],
-	[sg.Text('接收数据:', font=FONT_BUT_SIZE), sg.Button('清除接收数据', font=FONT_BUT_SIZE, key='-CLEARBUTTER-')],
+	[sg.Text('接收数据:', font=FONT_BUT_SIZE), sg.Button('清除接收数据', font=FONT_BUT_SIZE, key='-CLEAR_RECIPT-')],
 	[sg.Listbox('', size=FRAME_SIZE, key='-PB3-')]
 ]
 
 #整体布局
 Layout = [
-	[sg.Frame('', frame_layout1),sg.Frame('', frame_layout2)]
+   	[sg.Frame('', frame_layout1),sg.Frame('', frame_layout2)]
 ]
 
 window = sg.Window('串口调试工具', Layout)
+dict_butten = {
+	'打开串口':'-OPEN_GORGE-',
+    '关闭串口':'-CLOSE_GORGE-',
+    '发送':'-SEND_DATA-',
+    '接收':'-RECIPT_DATA-',
+    '清除发送数据':'-CLEAR-SENDDATA',
+    '清除接收数据':'-CLEAR_RECIPT-',
+}
+
+com = Communication('COM1', 9600, 6)
+
 while True:
     event, values = window.read()
     if event==sg.WINDOW_CLOSED or event=='Exit':
         break
-	# elif event == '打开串口':
-	# 	pass
+    if event == '-OPEN_GORGE-':
+        com.open_gorge()
+    if event == '-CLOSE_GORGE-':
+        com.close_gorge()
+    if event == '-SEND_DATA-':
+        result = com.send_messages(values['-INPUT_TEXT-'])
+        values['-PB2-'] = values['-INPUT_TEXT-']
+        window['-PB2-'].Update(values['-PB2-'])
+    if event == '-RECIPT_DATA-':
+        pass
+    # if event == '-CLEAR-SENDDATA':
+    # if event == '-CLEAR_RECIPT-':
+
+
+
+
+    print(event)
+    print(values)
 
 window.close()
 
