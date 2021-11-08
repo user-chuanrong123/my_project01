cc2c6e016d8592820a6a3a5872880bede5f78293
user-chuanrong123 <1021719031@qq.com>
2021-11-08 09:33:11 +0800

    串口调试第一个版本

---
+++ Python_Gorge/3333.py
@@ -0,0 +1,15 @@
+# _*_coding:utf-8_*_
+# @Time:2021/10/19 上午10:17
+# @Author :RANRAN
+# @Filename :3333.py
+# @software :PyCharm
+
+
+str = 'h'
+yy = ord(str)
+xx = chr(yy)
+# hex1 = hex(xx)
+print('yy type(yy)=', type(yy), yy)
+print('xx type(xx)=', type(xx), xx)
+# print(hex1)
+
